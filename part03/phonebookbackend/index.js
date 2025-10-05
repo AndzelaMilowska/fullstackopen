1419a8a542fb5b6fda8937e9a3f4be3941b76293
Nela <andzelamilowska@gmail.com>
2025-10-06 00:01:47 +0200

    added exercise 3.18

--- part03/phonebookbackend/index.js
+++ part03/phonebookbackend/index.js
@@ -44,32 +44,16 @@ function generateRandomNumber() {
 
 const Contact = require("./models/contact");
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 //get request for info page
 app.get("/info", (request, response) => {
-  let responseData = `<p>Phonebook has info for ${persons.length} people</p>`;
+  let length = 0;
+  Contact.find({}).then((contacts) => {
+      console.log(contacts);
+      length = contacts.length;
+      response.json(contacts);
+    });
+  let responseData = `<p>Phonebook has info for ${length} people</p>`;
   responseData += `<p>${new Date()}</p>`;
   response.send(responseData);
 });
