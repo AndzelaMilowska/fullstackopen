dc82586e15bfc287799a8b25e8830b061c45a1d5
Nela <andzelamilowska@gmail.com>
2025-11-14 14:04:02 +0100

    added ex 4.15-4.19

--- part04/BlogList/backend/app.js
+++ part04/BlogList/backend/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 const app = express()
@@ -10,5 +12,7 @@ const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
